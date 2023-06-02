cf6e0dfee079816c39cc9a37c0c8d0aa5460d60d
Scott <132684582+TomsScott@users.noreply.github.com>
2023-06-02 15:22:13 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -12,16 +12,14 @@ basic.clearScreen()
 servos.P2.setAngle(0)
 radio.setGroup(193)
 basic.forever(function () {
-    if (Lap <= 10) {
-        if (BitMaker.read_Din_value(GrovePort.P0) == 0) {
-            Lap += 1
-            radio.sendNumber(control.millis())
-            Time2 = control.millis()
-            basic.showNumber(Lap)
-            servos.P2.setAngle(5)
-            // Agitation de drapeau
-            basic.pause(300)
-            servos.P2.setAngle(0)
-        }
+    if (BitMaker.read_Din_value(GrovePort.P0) == 0) {
+        Lap += 1
+        radio.sendNumber(control.millis())
+        Time2 = control.millis()
+        basic.showNumber(Lap)
+        servos.P2.setAngle(5)
+        // Agitation de drapeau
+        basic.pause(300)
+        servos.P2.setAngle(0)
     }
 })
